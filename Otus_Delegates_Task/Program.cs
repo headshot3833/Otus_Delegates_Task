9240870d01ce0ad8b540f824a29f32fdc25da0c5
head <headshot_1998@mail.ru>
2024-10-25 14:56:48 +0400

    test

--- Otus_Delegates_Task/Program.cs
+++ Otus_Delegates_Task/Program.cs
@@ -9,9 +9,18 @@ namespace ConsoleApp
             FileSearcher searcher = new FileSearcher();
             searcher.FileFound += Searcher_Found;
 
-            searcher.SearchDirectory(@"C:\Users\head\source\repos\Otus_Delegates_Task\Otus_Delegates_Task");
-        }
 
+            searcher.SearchDirectory(@"C:\Users\head\source\repos\Otus_Delegates_Task\Otus_Delegates_Task");
+            var test = new List<Item>
+            {
+                new Item { Value = 1.0f },
+                new Item { Value = 3.5f },
+                new Item { Value = 2.2f }
+            };
+
+            var maxValue = test.GetMax(item => item.Value);
+            Console.WriteLine($"Максимальное значение: {maxValue?.Value}");
+        }
         private static void Searcher_Found(object sender, FileArgs e)
         {
             Console.WriteLine($"Файл нашли : {e.FileName}");
